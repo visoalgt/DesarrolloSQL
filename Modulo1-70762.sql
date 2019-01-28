c471f1ab3fcfaa6a652898d64212a269549debc3
vhcardenas <47119053+vhcardenas@users.noreply.github.com>
2019-01-28 12:39:08 -0600

    Revert "Explicación de Funciones"
    This reverts commit 94a4483d385cec2bdd4ffe695d0289932851391d.

--- Modulo1-70762.sql
+++
@@ -1,5 +0,0 @@
---Dependencias de otros objetos con el objeto que queremos borrar
-
-Select o.name, o.object_id, o.type_desc from sys.objects as o 
-inner join sys.sysdepends as d on o.object_id=d.id
-where depid=757577737
